1cb7822e992a5b4b58f5970778005bb14d93dc5b
DESKTOP-AUFDPEM\Will <will.petillo@gmail.com>
2018-05-03 17:11:48 -0700

    Initial commit

---
+++ Assets/Main/AudioController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] AudioSource[] sounds;
+    AudioClip[] clips;
+
+	void Start ()   //Initialize variables
+    {
+        clips = new AudioClip[sounds.Length];
+
+        for (int i = 0; i < sounds.Length; i++)
+            clips[i] = sounds[i].clip;
+	}
+	
+	void Update ()  //Get user input
+    {
+        if (Input.GetButtonDown("Track1"))
+            SoundTypeCheck(0);
+        if (Input.GetButtonDown("Track2"))
+            SoundTypeCheck(1);
+        if (Input.GetButtonDown("SFX1"))
+            SoundTypeCheck(2);
+        if (Input.GetButtonDown("SFX2"))
+            SoundTypeCheck(3);
+        if (Input.GetButtonDown("SFX3"))
+            SoundTypeCheck(4);
+    }
+
+    void SoundTypeCheck(int soundID)
+    {
+        if (sounds[soundID].loop)
+            PlaySong(soundID);
+        else
+            PlayEffect(soundID);
+    }
+
+    void PlaySong(int songID)
+    {
+        if (sounds[songID].isPlaying)
+            sounds[songID].Stop();
+        else
+            sounds[songID].Play();
+    }
+
+    void PlayEffect(int effectID)
+    {
+        sounds[effectID].PlayOneShot(clips[effectID], 2f);
+    }
+}
